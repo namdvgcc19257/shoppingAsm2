bbe865efae4bb57054b3a04f9c2afcdc124aab90
namdvgcc19257 <91666757+namdvgcc19257@users.noreply.github.com>
2021-11-10 19:15:01 +0700

    add
    1

--- Shopping/connection.php
+++ Shopping/connection.php
@@ -1,3 +1,7 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'shopping') or die("Can not connect database".mysqli_connect_error());
+    $conn = pg_connect("postgres://zlqgkvwwljmbqv:45ce6e6df159c01766c9ef1d239965b223b44de574be97d9aae19add549d8b31@ec2-54-167-201-170.compute-1.amazonaws.com:5432/d5lqnlt6fkcltd");
+
+    if(!$conn){
+        die("Can not connect database");
+    } 
 ?>
